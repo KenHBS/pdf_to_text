c1ae1b745ce3500be99bdebb466e36e3a0eb2c43
KenHBS <k.h.b.schroder@gmail.com>
2018-01-07 22:31:07 +0100

    raw setup pdf to csv

---
+++ pdf_to_csv.py
@@ -0,0 +1,113 @@
+# Transform PDFs in a folder to CSV files:
+ 
+import os, csv, re
+import PyPDF2
+from optparse import OptionParser
+
+
+def pdfs_2_txt(folder):
+    pdf_txts = []
+    removed_docs = []
+    os.chdir(folder)
+
+    files = os.listdir(folder)
+    docnames = [x for x in files if ".pdf" in x]
+    for docname in docnames:
+        print(docname)
+        print("----")
+        try:
+            pdf_txt = pdf_2_txt(docname)
+            if len(pdf_txt) > 0:
+                pdf_txts.append(pdf_txt)
+            else:
+                pass
+        except (OSError, PyPDF2.utils.PdfReadError) as a:
+            os.remove(docname)
+            print("Removed %s because of %s" % (docname, a))
+            removed_docs.append(docname)
+            continue
+    print("Removed the following the documents:")
+    print(removed_docs)
+    return pdf_txts
+
+
+def pdf_2_txt(docname):
+    file = open(docname, 'rb')
+    reader = PyPDF2.PdfFileReader(file)
+
+    n = reader.numPages
+    pdf_txt = ""
+    for i in range(n):
+        page = reader.getPage(i)
+        page_txt = " " + page.extractText()
+        pdf_txt = pdf_txt + page_txt
+    file.close()
+    return pdf_txt
+
+
+def clean_text(text):
+    pat1 = "https://.*?(?=\\n)"
+    pat2 = "\w+([^\x00-\x7F]|\w+"
+    pat3 = "\\n"
+    pat4 = "(\(JEL)+(.*?)\)+"
+    pat5 = " American.*?(:  [0-9]{3})"
+    patterns = [pat1, pat2, pat3, pat4, pat5]
+    for pat in patterns:
+        text = re.subn(pat, "", text)[0]
+    return text
+
+
+def get_id(text):
+    pat = "https://.*?(?=\\n)"
+    try:
+        return re.findall(pat, text)[0]
+    except IndexError:
+        return None
+
+
+def get_jel(text):
+    pat = "(?<=JEL )(.*?)(?=\))"
+    try:
+        jelcodes = re.findall(pat, text)[0]
+        return re.subn(",", "", jelcodes)[0]
+    except IndexError:
+        return None
+
+
+def main():
+    parser = OptionParser()
+    parser.add_option("-f", dest="folder",
+                      help="absolute folder path with PDF files")
+    parser.add_option("-o", dest="opt",
+                      help="0 creates single csv files, 1 generates"
+                           "one csv file for the LDA thesis data prep",
+                      default=1, type=int)
+    parser.add_option("-d", dest="docname",
+                      help="save results as (only for opt=1", 
+                      default="results.csv")
+    (options, args) = parser.parse_args()
+    if not options.folder:
+        parser.error("need to supply folder path (-f)")
+
+    texts = pdfs_2_txt(options.folder)
+    texts = [clean_text(text) for text in texts]
+    if options.opt == 1:
+        ids = [get_id(text) for text in texts]
+        jels = [get_jel(text) for text in texts]
+        result = list(zip(ids, texts, jels))
+        with open(options.docname, 'w') as result_file:
+            wr = csv.writer(result_file)
+            for line in result:
+                wr.writerow(list(line))
+    elif options.opt == 0:
+        files = os.listdir(options.folder)
+        files = [file for file in files if ".pdf" in file]
+        csv_names = [re.sub(".pdf", ".csv", file) for file in files]
+        for text, file in zip(texts, csv_names):
+            with open(file, 'w') as result_file:
+                wr = csv.writer(result_file)
+                wr.writerow([text])
+
+
+if __name__ == "__main__":
+    main()
