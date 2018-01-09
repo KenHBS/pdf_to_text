fac16a4fa7e73f6cc3e736fde06542141969a53a
KenHBS <k.h.b.schroder@gmail.com>
2018-01-09 17:06:23 +0100

    bug fix - windows newline argument

--- pdf_to_csv.py
+++ pdf_to_csv.py
@@ -11,8 +11,8 @@ def pdfs_2_txt(folder):
     files = os.listdir(folder)
     docnames = [x for x in files if ".pdf" in x]
     for docname in docnames:
-        print(docname)
         print("----")
+        print(docname)
         try:
             pdf_txt = pdf_2_txt(docname)
             if len(pdf_txt) > 0:
@@ -44,18 +44,17 @@ def pdf_2_txt(docname):
 
 
 def clean_text(text):
-    pat1 = "https://doi\..*?(?=\\n)"
-    pat2 = " American.*?(: [0-9]{1,4}).[0-9]{1,4}"
-    pat3 = "(\(JEL)+(.*?)\)+"
-    pat4 = "[^ a-zA-Z]"
-    patterns = [pat1, pat2, pat3, pat4]
+    pat1 = "https.*?(?=\\s)"
+    pat2 = "(\(JEL)+(.*?)\)+"
+    pat3 = "[^ a-zA-Z]"
+    patterns = [pat1, pat2, pat3]
     for pat in patterns:
         text = re.subn(pat, "", text)[0]
     return text
 
 
 def get_id(text):
-    pat = "https://doi\..*?(?=\\n)"
+    pat = "https://doi\..*?(?=\\s)"
     try:
         return re.findall(pat, text)[0]
     except IndexError:
@@ -86,24 +85,25 @@ def main():
         parser.error("need to supply folder path (-f)")
 
     texts = pdfs_2_txt(options.folder)
-    if options.opt == 1:
-        ids = [get_id(text) for text in texts]
-        jels = [get_jel(text) for text in texts]
-        texts = [clean_text(text) for text in texts]
-        result = list(zip(ids, texts, jels))
-        with open("AEA_fulldocs.csv", 'w') as result_file:
-            wr = csv.writer(result_file)
-            for line in result:
-                wr.writerow(list(line))
-    elif options.opt == 0:
+
+    if options.opt == 0:
         texts = [clean_text(text) for text in texts]
         files = os.listdir(options.folder)
         files = [file for file in files if ".pdf" in file]
         csv_names = [re.sub(".pdf", ".csv", file) for file in files]
         for text, file in zip(texts, csv_names):
-            with open(file, 'w') as result_file:
+            with open(file, 'w', newline="") as result_file:
                 wr = csv.writer(result_file)
                 wr.writerow([text])
+    elif options.opt == 1:
+        ids = [get_id(text) for text in texts]
+        jels = [get_jel(text) for text in texts]
+        texts = [clean_text(text) for text in texts]
+        result = list(zip(ids, texts, jels))
+        with open("AEA_fulldocs.csv", 'w', newline="") as result_file:
+            wr = csv.writer(result_file)
+            for line in result:
+                wr.writerow(list(line))
 
 
 if __name__ == "__main__":
